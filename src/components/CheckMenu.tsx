f71fa4ea91142297b887bdd03f747cee582322c2
Daniel Domínguez <danidomin19@gmail.com>
2024-12-05 22:35:59 +0100

    menu

--- src/components/CheckMenu.tsx
+++ src/components/CheckMenu.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import '../styles/CheckMenu.scss'
+import Link from 'next/link'
 
 const CheckMenu = () => {
   return (
     <div className='check-menu'>
       <div className='img-container'>
         <Image src='/gallery/burger1.jpg' alt='menu dak burger' height={800} width={800} />
-        <a href='#'>Consulta nuestro Menu</a>
+        <Link href='/menu'>Consulta nuestro Menu</Link>
       </div>
     </div>
   )
