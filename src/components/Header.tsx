5d024cfab21e6eb1c2651734c7149e2c9a978c6a
Daniel Domínguez <danidomin19@gmail.com>
2025-04-08 16:26:43 +0200

    menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,7 +62,7 @@ const Header = () => {
           <Rocket className='close' onClick={closeMenu} />
           <ul>
             <li><Link href='/' onClick={closeMenu}>Inicio</Link></li>
-            <li><Link href='/menu' onClick={closeMenu}>Menu</Link></li>
+            <li><Link href='/carta' onClick={closeMenu}>Menu</Link></li>
             <li><Link href='/conocenos' onClick={closeMenu}>Sobre Nosotros</Link></li>
             {/* <li><Link href='/retos' onClick={closeMenu}>Retos</Link></li> */}
             <li><a target='_blank' href='https://dakburgerlacalademijas.order.app.hd.digital/menus' onClick={closeMenu}>Pide a domicilio</a></li>
