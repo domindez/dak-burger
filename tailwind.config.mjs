0681ef8134f1f89c52529373057e50c874a9b830
domindez <danidomin19@gmail.com>
2026-02-19 20:43:55 +0100

    Refactor ScoreTape component to use loops for rendering items and update Tailwind CSS marquee animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -43,7 +43,7 @@ export default {
       keyframes: {
         marquee: {
           '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-50%)' },
+          '100%': { transform: 'translateX(-25%)' },
         },
         fadeIn: {
           '0%': { opacity: '0' },
